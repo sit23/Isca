ce9cb6864cc1042ca130ae520241b3d58fd76c56
sit23 <stephen.i.thomson@gmail.com>
2018-11-28 14:25:46 +0000

    Get mkmf template from the same place as the model.

--- postprocessing/plevel_interpolation/compile_plev_interpolation.sh
+++ postprocessing/plevel_interpolation/compile_plev_interpolation.sh
@@ -5,6 +5,8 @@ cd ./exec
 
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 
-../bin/mkmf -p plev.x -t ../bin/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
+compiler=${GFDL_MKMF_TEMPLATE:-ia64}
+
+../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/mkmf.template.${compiler} -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
 
 make -f Makefile
